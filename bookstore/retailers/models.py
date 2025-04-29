12a10634a0f871f0d9356b3beb3844a178a8308d
GOURINATH <mahatogourinathkumar@gmail.com>
2025-04-29 15:01:22 +0530

    hello

---
+++ bookstore/retailers/models.py
@@ -0,0 +1,13 @@
+# Create your models here.
+from django.db import models
+
+class Retailer(models.Model):
+    name = models.CharField(max_length=200)
+    mobile_no = models.CharField(max_length=100)
+    date_of_joining = models.DateField()
+    total_sales = models.DecimalField(max_digits=6, decimal_places=2)
+    address = models.CharField(max_length=300)
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
