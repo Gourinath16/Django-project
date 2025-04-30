36eac2ba8c08f12e7359b3ba2cf3ad6c1e101a91
GOURINATH <mahatogourinathkumar@gmail.com>
2025-04-30 14:21:18 +0530

    hello

--- bookstore/books/models.py
+++ bookstore/books/models.py
@@ -10,3 +10,13 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    message = models.TextField()
+    submitted_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.email}"
+
